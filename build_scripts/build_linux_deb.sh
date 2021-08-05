8dd211a03583844cf6349f0ed4ed365a8ef8b322
Olive-blockchain <87711356+Olive-blockchain@users.noreply.github.com>
2021-08-06 01:28:23 +0800

    new

--- build_scripts/build_linux_deb.sh
+++ build_scripts/build_linux_deb.sh
@@ -5,6 +5,7 @@ if [ ! "$1" ]; then
 	exit 1
 elif [ "$1" = "amd64" ]; then
 	PLATFORM="$1"
+	REDHAT_PLATFORM="x86_64"
 	DIR_NAME="olive-blockchain-linux-x64"
 else
 	PLATFORM="$1"
@@ -26,6 +27,7 @@ echo "Olive Installer Version is: $OLIVE_INSTALLER_VERSION"
 echo "Installing npm and electron packagers"
 npm install electron-packager -g
 npm install electron-installer-debian -g
+npm install electron-installer-redhat -g
 
 echo "Create dist/"
 rm -rf dist
@@ -76,6 +78,18 @@ LAST_EXIT_CODE=$?
 if [ "$LAST_EXIT_CODE" -ne 0 ]; then
 	echo >&2 "electron-installer-debian failed!"
 	exit $LAST_EXIT_CODE
+fi
+
++if [ "$REDHAT_PLATFORM" = "x86_64" ]; then
+	echo "Create olive-blockchain-$OLIVE_INSTALLER_VERSION.rpm"
+  electron-installer-redhat --src dist/$DIR_NAME/ --dest final_installer/ \
+  --arch "$REDHAT_PLATFORM" --options.version $OLIVE_INSTALLER_VERSION \
+  --license ../LICENSE
+  LAST_EXIT_CODE=$?
+  if [ "$LAST_EXIT_CODE" -ne 0 ]; then
+	  echo >&2 "electron-installer-redhat failed!"
+	  exit $LAST_EXIT_CODE
+  fi
 fi
 
 ls final_installer/
