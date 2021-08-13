34e4cd213e1bf40c908c0ac8a1327a6b5b83abdd
Olive-blockchain <87711356+Olive-blockchain@users.noreply.github.com>
2021-08-13 15:57:44 +0800

    dev

--- olive/full_node/full_node.py
+++ olive/full_node/full_node.py
@@ -185,7 +185,7 @@ class FullNode:
             default_port = None
         if "dns_servers" in self.config:
             dns_servers = self.config["dns_servers"]
-        elif self.config["port"] == 6333:
+        elif self.config["port"] == 7333:
             # If `dns_servers` misses from the `config`, hardcode it if we're running mainnet.
             dns_servers.append("dns-introducer.oliveblockchain.co")
         try:
